797dfa3f1efba1c4b8fe0250f6cb7a1b095694df
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-11 14:07:09 +0000

    Changed to Array

--- directory.rb
+++ directory.rb
@@ -1,28 +1,37 @@
+# first, we print a list of all the students
+students = ["Tom Groombridge",
+	"Tom Shacham",
+	"James Graham",
+	"Laura Young ",
+	"Nick  Keszler",
+	"Georgi Georgiev",
+	"Jean-Baptiste Blanc",
+	"Ken Scott",
+	"Anath Abensour",
+	"Erica Salvaneschi",
+	"Chris Hill",
+	"Asta Bevainyte",
+	"Hannah Knights",
+	"Simon Wolf",
+	"Gianni Guitteaud",
+	"James Brooke",
+	"Bruce Steedman",
+	"Jeremy Marer",
+	"Nisar Tahir",
+	"Giacomo Patella",
+	"Michael Fisher",
+	"Peter Kristo",
+	"Kumaran Veluppillai"
+
+
+
+
+
+]
 puts "Students from my Cohort"
 puts "======================="
-puts "Tom Groombridge"
-puts "Tom Shacham"
-puts "James Graham"
-puts "Laura Young "
-puts "Nick  Keszler"
-puts "Georgi Georgive"
-puts "Ken Scott"
-puts "Anath Abensour"
-puts "Erica Salvaneschi"
-puts "Gianni Guitteaud"
-puts "Chris Hill"
-puts "Asta Bevainyte"
-puts "Hannah Knights"
-puts "Simon Wolf"
-puts "Jean-Baptiste Blanc"
-puts "James Brooke"
-puts "Bruce Steedman"
-puts "Jeremy Marer"
-puts "Nisar Tahir"
-puts "Giacomo Patella"
-puts "Michael Fisher"
-puts "Peter Kristo"
-puts "Kumaran Veluppillai"
-
-num_student = 23
-puts "Overall, we have #{num_student} great students"
+# then we print the overall number of students
+students.each {	|student| 
+	puts student
+}
+puts "Overall, we have #{students.length} great students"
