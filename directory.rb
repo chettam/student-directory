3f96e86845fff14b92ac62e935a41c842d660a47
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-11 15:54:09 +0000

    added menu and interaction to directory

--- directory.rb
+++ directory.rb
@@ -1,9 +1,9 @@
 # first :cohort  => we print a list of all the students
-students = [
+@students = [
 	{:name =>"Tom Groombridge", :cohort  => :november},
 	{:name =>"Tom Shacham", :cohort  => :november},
 	{:name =>"James Graham", :cohort  => :november},
-	{:name =>"Laura Young ", :cohort  => :november},
+	{:name =>"Lara Young ", :cohort  => :november},
 	{:name =>"Nick  Keszler", :cohort  => :november},
 	{:name =>"Georgi Georgiev", :cohort  => :november},
 	{:name =>"Jean-Baptiste Blanc", :cohort  => :november},
@@ -26,8 +26,7 @@ students = [
 ]
 # definition to display the header 
 def print_header
-	puts "Students from my Cohort at Makers Academy"
-	puts "========================================="
+	
 
 end
 
@@ -42,7 +41,62 @@ end
 def  footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
+def menu_choice
+	puts "Welcome to the November Cohort directory"
+	puts "========================================"
+	puts "type 'Add' to add a student"
+	puts "type 'Del' to delete a student"
+	puts "type 'List'to list all the students"
+	puts "type 'Quit' to exit"
+end
+# gives user choice to manage the directory
+def menu
+	menu_choice
+	choice = gets.chomp
+	while choice != "Quit"
+		case choice 
+		when "Add"
+				add_student
+		when "Del"
+			delete_student
+		when "List"
+			list_students
+		else
+			puts "wrong choice"
+		end
+		menu_choice
+		choice = gets.chomp
+	end	
+end
+# def to list the students
+def list_students
+	puts "Here are the Students from my Cohort at Makers Academy"
+	puts "========================================="
+	@students.each  { |name|
+	 puts "#{name[:name]} (#{name[:cohort]} cohort)"
+	 }
+end	 
+# def to add a student
+def add_student
+	puts "Let's add a student"
+	puts "==================="
+	puts "Please enter a student name"
+	name = gets.chomp
+	puts "please enter a student cohort name"
+	cohort = gets.chomp
+	@students << {:name =>name , :cohort => cohort}
+	puts "the following student is added #{name} (#{cohort} cohort) "
+	list_students
+end
+#def to delete a student
+def delete_student
+	puts " Let's delete a student"
+	puts "======================="
+	puts "Please enter a student name"
+	name = gets.chomp
+	@students.delete_if { |student| student[:name] == name }
+ 	puts " #{name} has been removed from the directory (if it existed)"
+	list_students
+end
 
-print_header
-display(students)
-footer(students)
+menu
